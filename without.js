42ebe73aa99873aa2230e010bed20038c17755c5
Jason Kadman <jkadman231@gmail.com>
2022-10-24 19:50:27 +0000

    basic framework of a function using the instructions I provided, testing comes next

--- without.js
+++ without.js
@@ -18,3 +18,21 @@ const eqArrays = function(arr1, arr2) {
   }
   return true;
  }
+
+ const without = function(source, itemsToRemove) {
+  let unique = [];
+  for (let sources of source) {
+    if (sources !== itemsToRemove) {
+      unique.push(sources);
+    }
+  }
+  return unique;
+ }
+
+ /*
+This function should take in a source array and a itemsToRemove array. It should return a new array with only those elements from source that are not present in the itemsToRemove array.
+-- a for loop and if statement that move through both source and itemsToRemove
+-- check for match
+-- need to push items to a new array that will be printed.
+-- 
+ */
