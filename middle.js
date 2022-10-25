fdb2206efeced5690f0dfef9e6a1fc48c0a6c45b
Jason Kadman <jkadman231@gmail.com>
2022-10-25 22:43:50 +0000

    have the middle portions of the function working, for some reason having trouble with returning an empty array but the skeleton of the code is complete

---
+++ middle.js
@@ -0,0 +1,53 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🐵🐵🐵 Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`😆😆😆 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }  
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } 
+  }
+  return true;
+ }
+
+ let middle = function(array) {
+  let midpoint = [];
+  if (array.length < 2) {
+    return;
+  } else if (array.length % 2 !== 0) {
+    midpoint = (array.length) / 2;
+    midpoint = Math.ceil(midpoint);
+  } else {
+    let mid = array.length / 2;
+    midpoint = [array[mid - 1], array[mid]];
+  }
+  return midpoint;
+ }
+
+let small = [1, 2];
+let odd = [1, 2, 3, 4, 5];
+let even = [1, 2, 3, 4, 5, 6];
+console.log(middle([1, 2]));
+console.log(middle(odd));
+console.log(middle(even));
+
+
+ /*
+Implement middle which will take in an array and return the middle-most element(s) of the given array.
+For arrays with one or two elements, there is no middle. Return an empty array.
+For arrays with odd number of elements, an array containing a single middle element should be returned.
+For arrays with an even number of elements, an array containing the two elements in the middle should be returned
+
+Need an if statement to account for: array is < 3, array.length % 2 !== 0, array.length % 2 === 0
+how to find middle: odd : (length - 1) / 2; even: var mid = array.length / 2, midpoint = [array[var]. array[var - 1]]
+
+ */
