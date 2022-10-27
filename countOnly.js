4edf736206d4a0b8a85ad3184bac5b3e423fe285
Jason Kadman <crow@pop-os.localdomain>
2022-10-26 18:55:52 -0700

    Have a pretty good skeleton of a function, just need to figure out how to add the count to the keys of the new object

--- countOnly.js
+++ countOnly.js
@@ -15,14 +15,34 @@ so create object result{} that will store the items that match
 // Need to account for how I am going to count each item in allItems
 // If itemsToCount === true, item in allItems + 1
 // finally push to results item in allItems as key, and the amount of times item is found in allItems
+// const countOnly = function(allItems, itemsToCount) {
+//   let results = {};
+//   for (let item in allItems) {
+//   if (allItems[item] === item) /*&& itemsToCount.item === true)*/ {
+//     //results = results.item += 1;
+//     results = {a: 1, b: 2};
+//   }
+//  }
+//  return results;
+// }
 const countOnly = function(allItems, itemsToCount) {
-  let results = {};
-  if (allItems === itemsToCount) {
-    results.push()
+    let results = {};
+    let count = 0;
+    for (let item in allItems) {
+    if (allItems[item] && (itemsToCount[allItems[item]] === true)) {
+        allItems.forEach(element => {
+          results[element] = count += 1;
+          });
+    } 
   }
-  
+   return results;
 }
-
-const test = [1, 1, 2, 3, 3, 4, 5];
-const answer = countOnly(test, {1: true, 2: true, 3: false, 4:true});
+  
+//What is actually happening in this challenge, is we are looking at the key of the array which is a number and anaylizing the associated value with that key
+const test = ['Jason', 'bob', 'fred', 'leah', 'susan', 'sarah', 'Jason'];
+const test2 = {'Jason': true, 3: true, 'leah': true, 'susan': true, 4: false};
+// // const test = [1, 1, 2, 3, 3, 4, 5];
+// // const answer = countOnly(test, {1: true, 2: true, 3: false, 4:true});
+// const answer = {1: true, 2: true, 3: false, 4:true};
+console.log(countOnly(test, test2));
 
