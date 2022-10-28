d0c638617b749c91b390015d96de22d2fe2d8756
Jason Kadman <jkadman231@gmail.com>
2022-10-28 00:34:18 +0000

    almost the final version of my project, have to figure out how to use assertEquals

--- countLetters.js
+++ countLetters.js
@@ -1,22 +1,16 @@
 /*
 The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
 */
-/*
-const countLetters = function(string) {
-  let result = {};
-  for (let letter of string) {
-    if (letter !== ' ') {
-      if (result.hasOwnProperty(letter)) {
-      result[letter] += 1;
-      } else {
-        result[letter] = 1;
-      }
-    }
-  } 
-    return result;
-}; */
-  
-const countLetters = function(string) {
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `🐵🐵🐵 Assertion Passed: ${actual} === ${expected}`
+  } else {
+    return `😆😆😆 Assertion Failed: ${actual} !== ${expected}`
+  }
+};
+
+
+  const countLetters = function(string) {
   let result = {};
   for (let letter of string) {
     if (letter !== ' ') {
@@ -30,5 +24,6 @@ const countLetters = function(string) {
     return result;
 };
 
-//console.log(countLetters('Lighthouse'));
-console.log(countLetters('LHL'));
+console.log(countLetters('Lighthouse in the house!'));
+console.log(countLetters('LHL LHL'));
+console.log(assertEqual(countLetters('LHL'), { L: 2 }))
