d5fb2439e2ca9320757c74c86d6d43c5a7a7bf01
Jason Kadman <jkadman231@gmail.com>
2022-10-29 00:28:37 +0000

    realized that assertArrayEqual didn't work for multidimensional arrays so added code to flatten the arrays before they are tested for equality

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,28 +6,28 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
+  let newArr1 = arr1.flat();
+  let newArr2 = arr2.flat();
+  if (newArr1.length !== newArr2.length) {
     return false;
   }  
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    if (newArr1[i] !== newArr2[i]) {
       return false;
     } 
   }
   return true;
- }
+ } 
 
+console.log(assertArraysEqual([1, 2, 3], [1, [2, 3]])) // => true
 
-//eqArrays(assertArraysEqual([1, 2, 3], [1, 2, 3])) // => true
+ //assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+// assertArraysEqual([1, 2, 3], [1, 2, 3, 5])
+// assertArraysEqual([1, 2, '3'], [1, 2, 3, 5])
 
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
-assertArraysEqual([1, 2, 3], [1, 2, 3, 5])
-assertArraysEqual([1, 2, '3'], [1, 2, 3, 5])
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS 
-assertArraysEqual([], [3, 2, 1])
+//console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => should PASS 
+// assertArraysEqual([], [3, 2, 1])
