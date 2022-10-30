33cd5afec976d5373566e05087af199c9dbad4de
Jason Kadman <jkadman231@gmail.com>
2022-10-30 20:27:45 +0000

    finished the program, tested and added eqObjects and assertObjectsEqual.  Doesn't work really well with eqObjects, as the latter can't convert undefined or null.  But a problem for another day

--- findKey.js
+++ findKey.js
@@ -7,28 +7,74 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
+
+
+const eqObjects = function(object1, object2) {
+  let arr1 = Object.keys(object1);
+  let arr2 = Object.keys(object2);
+  if ((arr1, arr2)); {
+    if (arr1.length !== arr2.length) {
+      return false;
+    } for (let key of arr1) {
+      if (Array.isArray(key) || Array.isArray(object2[key])) {
+        return eqArrays(object1[key], object2[key]);
+      } else {
+        if (object1[key] !== object2[key])
+        return false;
+        } 
+      }
+return true;
+  }
+};
+
 /*
 Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
 */
 
+
+
 const findKey = function(object, callback) {
   for (let key in object) {
-    console.log(key)
-    let x = callback(key);
-    console.log(x);
-    if (object[key] === x) {
-      return object(key);
-    } else {
-      return undefined;
-    }
+    //console.log(key)
+    //console.log(object[key])
+    let x = callback(object[key]);
+    if (x) {
+      return key
+    } /*else {
+      return undefined; 
+    } */
+    // undefined or null won't run through eqObjects but findkey does return undefined.
   }
 }
 
-console.log(findKey({
+console.log(assertObjectsEqual(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)) // => "noma"
+}, x => x.stars === 2), 'noma')) // => "noma"
+
+console.log(assertObjectsEqual(findKey({
+  Jenny : { role: 'player' },
+  Jeremy: { role:'player' },
+  Emma:   { role: 'GM' },
+  Gary:   { role: 'player' }
+}, y => y.role === 'GM'), ''));
+
+// console.log(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2), 'karl') // => "noma"
+
+// console.log(findKey({
+//   Jenny : { role: 'player' },
+//   Jeremy: { role:'player' },
+//   Emma:   { role: 'GM' },
+//   Gary:   { role: 'player' }
+// }, y => y.role === 'GM'), 'GM');
