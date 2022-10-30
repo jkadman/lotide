7648ff2db086f8b1b44e498a7fe9e71d94155cd5
Jason Kadman <jkadman231@gmail.com>
2022-10-30 00:22:07 +0000

    finished testing, program works

--- takeUntil.js
+++ takeUntil.js
@@ -46,9 +46,26 @@ const takeUntil = function(array, callback) {
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2 ]);
+
+console.log('---');
+
+const data1Fail = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1Fail = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(results1Fail, [1, 2, 5, 7, 2, 4, 6 ]);
+
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+assertArraysEqual(results2,["I've", 'been', 'to', 'Hollywood'] );
+
+console.log('---');
+
+const data2Fail = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2Fail = takeUntil(data2, x => x === ',');
+console.log(results2);
+assertArraysEqual(results2,["I've", 'been', 'to', 'Hollywood', 'been'] );
