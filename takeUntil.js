447f76aa9e3b3d22786297b523ae2b9bb4b32ec7
Jason Kadman <jkadman231@gmail.com>
2022-10-30 00:18:29 +0000

    forgot to commit earlier versions, but have the function working am just starting to add in assertArrays etc

---
+++ takeUntil.js
@@ -0,0 +1,54 @@
+/*
+Implement takeUntil which will keep collecting items from a provided array until the callback provided returns a truthy value.
+*/
+
+// need an array to push the results to
+// need to loop through the array 
+// need an if statement where if the call back is triggered, the loop stops
+// return new array
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🐵🐵🐵 Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`😆😆😆 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let newArr1 = arr1.flat();
+  let newArr2 = arr2.flat();
+  if (newArr1.length !== newArr2.length) {
+    return false;
+  }  
+  for (let i = 0; i < arr1.length; i++) {
+    if (newArr1[i] !== newArr2[i]) {
+      return false;
+    } 
+  }
+  return true;
+ } 
+
+const takeUntil = function(array, callback) {
+  let newArr = [];
+  for (arr of array) {
+    let x = callback(arr);
+    if (!x) {
+      newArr.push(arr);
+    } else {
+      return newArr;
+    }
+  } 
+  return newArr;
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
