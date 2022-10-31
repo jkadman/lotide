07ade6399fc9ba51553a75bdb31cabfd5cb7e180
Jason Kadman <jkadman231@gmail.com>
2022-10-30 18:35:12 -0700

    making sure the program conforms to coding standards I have learned in the next week

--- tail.js
+++ tail.js
@@ -2,28 +2,28 @@
 Create a function tail which returns the "tail" of an array: everything except for the first item (head) of the provided array.
 
 create function tail
-create new array and push everything by arr[0] to new array
+create new array and push everything but arr[0] to new array
 ensure that the original array is not modified and that the new array contains everything but the first element of the original array
 
 */
 
-/*
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     return `🐵🐵🐵 Assertion Passed: ${actual} === ${expected}`
   } else {
     return `😆😆😆 Assertion Failed: ${actual} !== ${expected}`
   }
-}; */
-let bob = [1, 2, 3, 4];
-let friend = [1];
+}; 
 
 const tail = function(restArr) {
   const remaining = restArr.slice(1);
   return remaining;
 };
 
-console.log(bob);
-console.log((tail(bob)));
-console.log(tail(friend));
+const bob = [1, 2, 3, 4];
+const friend = [1];
+
+console.log(assertEqual(((tail(bob))), [2, 3, 4]));
+console.log(assertEqual(tail(friend)), []);
 console.log(tail([]));
