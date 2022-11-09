6204477850865c4f42d89a1935eecfbebd3cdbd2
Jason Kadman <jkadman231@gmail.com>
2022-11-09 17:18:08 +0000

    adding modules and test file

--- tail.js
+++ tail.js
@@ -4,26 +4,14 @@ Create a function tail which returns the "tail" of an array: everything except f
 create function tail
 create new array and push everything but arr[0] to new array
 ensure that the original array is not modified and that the new array contains everything but the first element of the original array
-
 */
 
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `🐵🐵🐵 Assertion Passed: ${actual} === ${expected}`
-  } else {
-    return `😆😆😆 Assertion Failed: ${actual} !== ${expected}`
-  }
-}; 
+const assertEqual = require('./assertEqual');
 
 const tail = function(restArr) {
   const remaining = restArr.slice(1);
   return remaining;
 };
 
-const bob = [1, 2, 3, 4];
-const friend = [1];
+module.exports = tail;
 
-console.log(assertEqual(((tail(bob))), [2, 3, 4]));
-console.log(assertEqual(tail(friend)), []);
-console.log(tail([]));
