511bde919a12faaba8c117f9f0e1346cf121e6eb
Jason Kadman <jkadman231@gmail.com>
2022-11-10 17:18:48 +0000

    my index file for lotide module exports

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 const flatten = require('./flatten');
+const eqObjects = require('./eqObjects');
+const countOnly = require('./countOnly');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  eqArrays: eqArrays,
-  flatten: flatten;
+  countOnly,
+  eqObjects,
+  flatten,
+  head,
+  tail,
+  letterPositions,
+  findKeyByValue,
+  middle,
+  eqArrays
 };
